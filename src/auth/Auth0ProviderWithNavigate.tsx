123e9f0d981561add4e58f94b8af069fbe4952aa
tlHoang <tlhh232003@gmail.com>
2024-10-17 05:18:56 +0700

    auth

--- src/auth/Auth0ProviderWithNavigate.tsx
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,21 +1,23 @@
-import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   children: React.ReactNode;
 }
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
+  const navigate = useNavigate();
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+  const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
-  if (!domain || !clientId || !redirectUri) {
+  if (!domain || !clientId || !redirectUri || !audience) {
     throw new Error("unable to load auth0 config");
   }
 
-  const onRedirectCallback = (appState?: AppState, user?: User) => {
-    console.log("APP STATE", appState);
-    console.log("USER", user);
+  const onRedirectCallback = () => {
+    navigate("/auth-callback");
   }
 
   return (
@@ -24,6 +26,7 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
       clientId={clientId}
       authorizationParams={{
         redirect_uri: redirectUri,
+        audience,
       }}
       onRedirectCallback={onRedirectCallback}
     >
