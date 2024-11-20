c6b9602f2e1f972ddf906f407a7643587f978a24
tlHoang <tlhh232003@gmail.com>
2024-11-20 15:26:33 +0700

    user order history

--- src/pages/OrderStatusPage.tsx
+++ src/pages/OrderStatusPage.tsx
@@ -2,6 +2,7 @@ import { useGetMyOrders } from "@/api/OrderApi";
 import OrderStatusDetail from "@/components/OrderStatusDetail";
 import OrderStatusHeader from "@/components/OrderStatusHeader";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const OrderStatusPage = () => {
   const { orders, isLoading } = useGetMyOrders();
@@ -14,26 +15,55 @@ const OrderStatusPage = () => {
     return "Không có đơn";
   }
 
-  // Filter out orders with status "placed"
-  const filteredOrders = orders.filter(order => order.status !== "placed");
+  // Filter orders based on their status
+  const activeOrders = orders.filter(order => order.status !== "delivered" && order.status !== "placed");
+  const doneOrders = orders.filter(order => order.status === "delivered");
 
   return (
-    <div className="space-y-10">
-      {filteredOrders.map((order) => (
-        <div className="space-y-10 bg-gray-50 p-10 rounded-lg">
-          <OrderStatusHeader order={order} />
-          <div className="grid gap-10 md:grid-cols-2">
-            <OrderStatusDetail order={order} />
-            <AspectRatio ratio={16 / 5}>
-              <img
-                src={order.restaurant.imageUrl}
-                className="rounded-md object-cover h-full w-full"
-              />
-            </AspectRatio>
+    <>
+      <Tabs defaultValue="active">
+        <TabsList>
+          <TabsTrigger value="active">Đang xử lý</TabsTrigger>
+          <TabsTrigger value="done">Đã giao</TabsTrigger>
+        </TabsList>
+        <TabsContent value="active">
+          <div className="space-y-10">
+            {activeOrders.map((order) => (
+              <div className="space-y-10 bg-gray-50 p-10 rounded-lg">
+                <OrderStatusHeader order={order} />
+                <div className="grid gap-10 md:grid-cols-2">
+                  <OrderStatusDetail order={order} />
+                  <AspectRatio ratio={16 / 5}>
+                    <img
+                      src={order.restaurant.imageUrl}
+                      className="rounded-md object-cover h-full w-full"
+                    />
+                  </AspectRatio>
+                </div>
+              </div>
+            ))}
           </div>
-        </div>
-      ))}
-    </div>
+        </TabsContent>
+        <TabsContent value="done">
+          <div className="space-y-10">
+            {doneOrders.map((order) => (
+              <div className="space-y-10 bg-gray-50 p-10 rounded-lg">
+                <OrderStatusHeader order={order} />
+                <div className="grid gap-10 md:grid-cols-2">
+                  <OrderStatusDetail order={order} />
+                  <AspectRatio ratio={16 / 5}>
+                    <img
+                      src={order.restaurant.imageUrl}
+                      className="rounded-md object-cover h-full w-full"
+                    />
+                  </AspectRatio>
+                </div>
+              </div>
+            ))}
+          </div>
+        </TabsContent>
+      </Tabs>
+    </>
   );
 };
 
